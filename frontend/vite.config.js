7ca537264de02a78c305a7b7d6f463b534c5e58d
Welles Jr <welles-junior@hotmail.com>
2026-02-13 20:01:47 -0300

    feat: Criando docker para nao precisar de dependencias instaladas

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,4 +9,14 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
 });
